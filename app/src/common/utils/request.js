471357857c1fd70d2dce6df918791d67687fc1ed
Kelvin Mai <kelvin.mai@oocl.com>
2021-06-26 13:32:00 +0800

    refactor

--- app/src/common/utils/request.js
+++ app/src/common/utils/request.js
@@ -1,31 +1,68 @@
-const baseUrl = 'https://unidemo.dcloud.net.cn/api'
-const request = ({ url = '', method = 'GET', date = {}, header = {} }) => {
+const http = ({
+  url = '',
+  method = 'GET',
+  data = {},
+  header = {},
+  showLoading = true,
+  loadingTitle = 'loading'
+}) => {
+  const token = uni.getStorageSync('accessToken')
+  const defaultHeader = {
+    'Content-Type': 'application/json',
+    Authorization: token ? 'Bearer ' + token : ''
+  }
+  header = {
+    ...defaultHeader,
+    ...header
+  }
   return new Promise((resolve, reject) => {
-    uni
-      .request({
-        method: method,
-        url: baseUrl + url,
-        data: date,
-        header: header,
-        dataType: 'json'
+    if (showLoading) {
+      uni.showLoading({
+        title: loadingTitle,
+        mask: true
       })
-      .then((response) => {
-        setTimeout(function () {
+    }
+    uni.request({
+      url: url,
+      data: data,
+      header: header,
+      timeout: 10000,
+      method: method,
+      dataType: 'json',
+      success: res => {
+        if (res.statusCode == 200) {
+          resolve(res.data)
+        } else if (res.statusCode == 401) {
+          reject(new Error('User Not Authorized'))
+        } else {
+          reject(new Error('server error'))
+        }
+      },
+      complete: () => {
+        if (showLoading) {
           uni.hideLoading()
-        }, 200)
-        resolve(response[1].data)
-      })
-      .catch((error) => {
-        reject(error[0])
-      })
+        }
+      },
+      fail: () => {
+        reject(new Error('request failed'))
+      }
+    })
   })
 }
-
-const requestObj = {
-  get: (url, params = {}) => {
-    return request({ url, data: params })
+const request = {
+  get: ({ url, params = {}, header = {}, showLoading = true, loadingTitle = 'loading' }) => {
+    return http({ url, data: params, header, showLoading, loadingTitle })
   },
-  request
+  post: ({ url, data = {}, header = {}, showLoading = true, loadingTitle = 'loading' }) => {
+    return http({ url, method: 'POST', data, header, showLoading, loadingTitle })
+  },
+  put: ({ url, data = {}, header = {}, showLoading = true, loadingTitle = 'loading' }) => {
+    return http({ url, method: 'PUT', data, header, showLoading, loadingTitle })
+  },
+  delete: ({ url, data = {}, header = {}, showLoading = true, loadingTitle = 'loading' }) => {
+    return http({ url, method: 'DELETE', data, header, showLoading, loadingTitle })
+  },
+  request: http
 }
 
-export default requestObj
+export default request
